c1ca44712936946f9ef12e7e579271f06093d7b4
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2021-11-14 21:35:42 -0500

    partial complete save function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,39 @@
 var currentDate = moment().format("MMMM Do YYYY");
 
 $(".date").text(currentDate);
+
+//var for saved plans object
+var plans = {};
+
+// load plans from local storage
+var loadPlans = function () {
+    plans = JSON.parse(localStorage.getItem("plans"));
+
+    // if nothing in localStorage, create an object for plans
+    if (!plans) {
+        plans = {
+           nine: [],
+           ten: [],
+           eleven: [],
+           twelve: [],
+           one: [],
+           two: [],
+           three: [],
+           four: [],
+           five: [] 
+        };
+    };
+};
+
+// save plans
+var save = function() {
+    $(event.target).siblings("textarea").val.push(plans)
+    localStorage.setItem("plans", JSON.stringify(plans));
+}
+
+$(".save").on("click", function() {
+    ($(event.target).siblings("textarea"))
+    
+}) 
+
+loadPlans();
