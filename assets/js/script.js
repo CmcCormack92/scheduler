eb84389cfbcef6667301acdd573a7258246bc5c9
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2021-11-16 17:39:45 -0500

    colors change based off past, present, future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,65 @@
 //get current date
-var currentDate = moment().format("MMMM Do YYYY");
-
-$(".date").text(currentDate);
-
-//var for saved plans object
-var plans = {};
-
-// load plans from local storage
-var loadPlans = function () {
-    plans = JSON.parse(localStorage.getItem("plans"));
-
-    // if nothing in localStorage, create an object for plans
-    if (!plans) {
-        plans = {
-           nine: [],
-           ten: [],
-           eleven: [],
-           twelve: [],
-           one: [],
-           two: [],
-           three: [],
-           four: [],
-           five: [] 
-        };
-    };
+var currentDate = function() {
+     var today = moment().format("MMMM Do YYYY");
+     $(".date").text(today);
 };
 
+//set current date
+currentDate();
+
+setInterval(currentDate, (1000 * 60) * 300);
+
+//change color based off time
+var backgroundColors = function() {
+    var hourId = $(".content").each(function() {
+
+        if ($(this).prop("id") == moment().format("HH")) {
+            $(this).addClass("bg-danger")
+            console.log($(this))
+        }   
+        else if ($(this).prop("id") < moment().format("HH")){
+            $(this).addClass("bg-secondary")
+            console.log($(this))
+        }
+        else if ($(this).prop("id") > moment().format("HH")){
+            $(this).addClass("bg-success")
+            console.log($(this))
+        }
+    });
+
+};
+
+setInterval(backgroundColors, (1000 * 60) * 5);
+
+//var for saved plans object
+var plans = [];
+
 // save plans
-var save = function() {
-    $(event.target).siblings("textarea").val.push(plans)
-    localStorage.setItem("plans", JSON.stringify(plans));
-}
+// var save = function() {
+//     var plan = $(event.target).siblings("textarea").val()
+
+//     var storage = JSON.parse(localStorage.getItem("plans"));
+
+//     // if nothing in localStorage, create an object for plans
+//     if (!storage) {
+//         storage = [];
+//     }
+
+//     var hourTask = [{
+//         task: plan
+//     }]
+
+//     storage.push(hourTask)
+//     localStorage.setItem("hour", JSON.stringify(storage))
+//     plans.push(storage)
+
+
+//     console.log(plans)
+// };
 
 $(".save").on("click", function() {
-    ($(event.target).siblings("textarea"))
+    save();
     
 }) 
 
-loadPlans();
+backgroundColors()
