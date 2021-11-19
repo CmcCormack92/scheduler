eef51a59be6aeb9a8aacc4af7d80cacb1c74a2d4
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2021-11-19 11:05:42 -0500

    all ids and text areas pushing to storage array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,69 @@
 //get current date
-var currentDate = function() {
-     var today = moment().format("MMMM Do YYYY");
-     $(".date").text(today);
+var currentDate = function () {
+    var today = moment().format("MMMM Do YYYY");
+    $(".date").text(today);
 };
 
 //change color based off time
-var backgroundColors = function() {
-    var hourId = $(".content").each(function() {
+var backgroundColors = function () {
+    var hourId = $(".content").each(function () {
         $(this).removeClass("bg-danger")
         $(this).removeClass("bg-secondary")
         $(this).removeClass("bg-success")
 
         if ($(this).prop("id") == moment().format("HH")) {
             $(this).addClass("bg-danger")
-        }   
-        else if ($(this).prop("id") < moment().format("HH")){
+        }
+        else if ($(this).prop("id") < moment().format("HH")) {
             $(this).addClass("bg-secondary")
         }
-        else if ($(this).prop("id") > moment().format("HH")){
+        else if ($(this).prop("id") > moment().format("HH")) {
             $(this).addClass("bg-success")
         }
-        
+
     });
 
 };
 
-//var for saved plans object
-var plans = [];
+// gets items from local storage
+var storage = JSON.parse(localStorage.getItem("eachHour"));
 
-var save = function() {
-    var text = $(event.target).siblings("textarea").val()
-    var hour = $(event.target).parent().prop("id")
-    
-    var storage = JSON.parse(localStorage.getItem("eachHour"));
+
+var save = function () {
+
+    localStorage.setItem("eachHour", JSON.stringify(storage))
+};
+
+var getItems = function () {
 
     // if nothing in localStorage, create an object for plans
     if (storage === null) {
         storage = []
     };
 
-    var hourTask = {
-        text: text,
-        hour: hour
-    }
+    // sets values for text and hour in local storage
+    $("input").each(function () {
+        var hourTask = {
+            text: $(this).val(),
+            hour: $(this).prop("id")
+        };
+        storage.push(hourTask)
+    });
 
-    storage.push(hourTask)
-    localStorage.setItem("eachHour", JSON.stringify(storage))
-    
-    plans.push(storage);
+    // pushes hour task object into storage array
+    storage.sort();
 
+    save();
+    console.log(storage)
+
+    for (var i = 0; i < storage.length; i++) {
+        $("input").text(storage[0].text)
+    };
 };
 
+
 // calls save function when save button is clicked
-$(".save").on("click", save)
+$(".save").on("click", getItems);
 
 //checks date every 5 hours to ensure it will show the correct date during work hours
 setInterval(currentDate, (1000 * 60) * 300);
@@ -64,4 +75,6 @@ setInterval(backgroundColors, (1000 * 60) * 2);
 currentDate();
 
 //get background colors
-backgroundColors()
+backgroundColors();
+
+getItems();
